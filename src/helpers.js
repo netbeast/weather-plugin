3c73ce9a242b6579c28b3848d8fa5b7110ccbdda
Cayetano Rodríguez Medina <Caye@MacBook-Pro-de-Cayetano.local>
2016-04-20 13:43:10 +0200

    Added helpers and created test folder

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+var helper = module.exports = {}
+var ApiError = require('./api-error')
+
+var weatherAux
+
+helper.temperature = function (xxxxx, done) {
+  
+    if (err) return done(err)
+    return done(null, temperature.toFixed(2))
+  })
+}
+
+helper.humidity = function (xxxxx, done) {
+  
+    if (err) return done(err)
+    return done(null, humidity.toFixed(2))
+  })
+}
