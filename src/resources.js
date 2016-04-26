09dd177f406a20d01aa726749bd22e626e5329d4
Cayetano Rodríguez Medina <Caye@MacBook-Pro-de-Cayetano.local>
2016-04-26 12:16:42 +0200

    Back to resources.js first version

--- src/resources.js
+++ src/resources.js
@@ -30,58 +30,59 @@ module.exports = function (callback) {
     })
 
   // Implement the device discovery method
-  // weather.discoverAll(function (device) {
-  // When we find a device
-  //  1. Look if its already exists on the database.
-  // var indx = objects.indexOf('/weather/id') // hook == /Namebrand/id. Example. /hueLights, /Sonos
-  // if (indx >= 0) {
-  //  objects.splice(indx, 1)
-  // } else {
-  //  Use this block to register the found device on the netbeast database
-  //  in order to using it later
-  request.post({url: API,
-    json: {
-      app: 'weather', // Name of the device brand
-      location: 'none',
-      topic: 'temperature', // lights, bridge, switch, temperature, sounds, etc
-      groupname: 'none',
-      hook: '/temperature/1' // HOOK == /Namebrand  Example. /hueLights, /Sonos
-  // We will use the id to access to the device and modify it.
-  // Any value to refer this device (MacAddress, for example) can work as id
-  }},
-    function (err, resp, body) {
-      if (err) return callback(err)
-      callback
-    })
+  weather.discoverAll(function (device) {
+    // When we find a device
+    //  1. Look if its already exists on the database.
+    var indx = objects.indexOf('/weather/id') // hook == /Namebrand/id. Example. /hueLights, /Sonos
+    if (indx >= 0) {
+      objects.splice(indx, 1)
+    } else {
+      //  Use this block to register the found device on the netbeast database
+      //  in order to using it later
+      request.post({url: API,
+        json: {
+          app: 'weather', // Name of the device brand
+          location: 'none',
+          topic: 'temperature', // lights, bridge, switch, temperature, sounds, etc
+          groupname: 'none',
+          hook: '/temperature/1' // HOOK == /Namebrand  Example. /hueLights, /Sonos
+      // We will use the id to access to the device and modify it.
+      // Any value to refer this device (MacAddress, for example) can work as id
+      }},
+        function (err, resp, body) {
+          if (err) return callback(err)
+          callback
+        })
 
-  request.post({url: API,
-    json: {
-      app: 'weather',
-      location: 'none',
-      topic: 'humidity',
-      groupname: 'none',
-      hook: '/humidity/1'
-  }},
-    function (err, resp, body) {
-      if (err) return callback(err)
-      callback
-    })
+      request.post({url: API,
+        json: {
+          app: 'weather',
+          location: 'none',
+          topic: 'humidity',
+          groupname: 'none',
+          hook: '/humidity/1'
+      }},
+        function (err, resp, body) {
+          if (err) return callback(err)
+          callback
+        })
+    }
+  })
+
+  setTimeout(function () {
+    weather.stopDiscoverAll(callback)
+    if (objects.length > 0) {
+      objects.forEach(function (hooks) {
+        request.del(API + '?hook=/temperature/' + hooks,
+          function (err, resp, body) {
+            if (err) callback(err)
+          })
+        request.del(API + '?hook=/humidity/' + hooks,
+          function (err, resp, body) {
+            if (err) callback(err)
+          })
+      })
+    }
+    callback(null, devices)
+  }, 15000)
 }
-// })
-
-setTimeout(function () {
-  // weather.stopDiscoverAll(callback)
-  if (objects.length > 0) {
-    objects.forEach(function (hooks) {
-      request.del(API + '?hook=/temperature/' + hooks,
-        function (err, resp, body) {
-          // if (err) callback(err)
-        })
-      request.del(API + '?hook=/humidity/' + hooks,
-        function (err, resp, body) {
-          // if (err) callback(err)
-        })
-    })
-  }
-// callback(null, devices)
-}, 15000)
